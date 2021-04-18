5c373d5ee0c77c97118f924dd73206802bb8c418
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-18 11:03:10 -0500

    commit tree 9c9dd6818ca8ff33af998c7452dc4104326a4b01

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+      const friendsArr = [...state.friends]
+      const friendToRemove = friendsArr.findIndex(f => f.id === action.id)
+      if (friendToRemove !== -1) {
+        friendsArr.splice(friendToRemove, 1)
+        return {friends: friendsArr}
+      }
+    default:
+      return state
+  }
 }
